62de9751ac45b53164c255b40292d4112eabc208
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2023-12-21 21:54:08 +0545

    Square root of given number

---
+++ sqrt.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+	float num, num1;
+	printf("Enter a number\n");
+	scanf("%f",&num);
+	num1=sqrt(num);
+	printf("Sqaure root is: %f",num1);
+	return 0;
+}
