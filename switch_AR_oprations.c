a3d25709819b553723c0e2f36412a00d4d55cec2
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2023-12-26 23:02:27 +0545

    SWITCH_AR_OPERATIONS

--- switch_AR_oprations.c
+++ switch_AR_oprations.c
@@ -4,36 +4,31 @@ int main(){
 	printf("Choose among options below ( 1-4 )\n");
 	printf(" 1) Additions \t 2) Subtractions\n 3) Multiplications \t 4) Division\n");
 	scanf("%d",&oper);
-	switch (oper){
-		case 1: {
-			int x,y;
+	int x,y;
 			printf("Enter 2 number\n");
 			scanf("%d%d",&x,&y);
+	switch (oper){
+		case 1: {
+			
 			int sum = x+y;
 			printf("Sum is %d",sum);
 			break;
 		}
 		case 2: {
-			int x,y;
-			printf("Enter 2 number\n");
-			scanf("%d%d",&x,&y);
+			
 			int sub = x-y;
 			printf("Subtract is %d",sub);
 			
 			break;
 		}
 		case 3: {
-			int x,y;
-			printf("Enter 2 number\n");
-			scanf("%d%d",&x,&y);
+			
 			int mul=x*y;
 			printf("Multiply is : %d ",mul);
 			break;
 		}
 		case 4: {
-			int x,y;
-			printf("Enter 2 number\n");
-			scanf("%d%d",&x,&y);
+			
 			float div = (float)x/(float)y;
 			printf("Divide is : %f",div);
 			break;
