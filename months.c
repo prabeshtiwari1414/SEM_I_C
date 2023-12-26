4de84c025c90e8124615d0f1eed6fd34bef99436
Prabesh Tiwari <prabeshtiwari1414@gmail.com>
2023-12-26 22:31:41 +0545

    MONTHS

---
+++ months.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+int main(){
+	int mth;
+	printf("Enter a number between 0-12 to find months\n");
+	scanf("%d",&mth);
+	switch (mth) {
+		case 1: {
+			printf("January");
+			break;
+		}
+		case 2 :{
+			printf("February");
+			break;
+		}
+		case 3 :{
+			printf("March");
+			break;
+		}
+		case 4 :{
+			printf("April");
+			break;
+		}
+		case 5 :{
+			printf("May");
+			break;
+		}
+		case 6 :{
+			printf("June");
+			break;
+		}
+		case 7 :{
+			printf("July");
+			break;
+		}
+		case 8 :{
+			printf("August");
+			break;
+		}
+		case 9 :{
+			printf("September");
+			break;
+		}
+		case 10 :{
+			printf("October");
+			break;
+		}
+		case 11 :{
+			printf("November");
+			break;
+		}
+		case 12 :{
+			printf("December");
+			break;
+		}
+		
+	}
+	return 0;
+}
